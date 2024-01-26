7bbcc7498ebb657a695354bfb7d5a37011aea48a
Rodrigo <rodrigocaiokell@gmail.com>
2024-01-26 11:42:03 -0300

    comitt top

--- js/teste.py
+++ js/teste.py
@@ -1,20 +1,34 @@
 from rembg import remove
 import urllib.request
 from PIL import Image
-import sys
 
 # Obter os parâmetros passados do Node.js
 urllib.request.urlretrieve( 
     'https://drive.google.com/uc?id=1ryneFJLyt23ZfVDMrpEoxXbZUKdBHrlk', 
     "gfg.png") 
-output_path = "C:/Users/rkoelln/Desktop/imagemsemfundo.png"
 
-print("estou aquiiii")
-# Carregar a imagem
-input_image = Image.open("gfg.png")
+# Caminho de saída para a imagem resultante
+output_path = "C:/LoveID/js/imagens/imagem_final.png"
+
+# Caminho para a imagem de fundo
+imagemFundoPath = "C:/LoveID/js/imagens/teste.jpg"
+
+# Carregar a imagem original
+imagemOriginal = Image.open("gfg.png")
 
 # Remover o fundo usando rembg
-output_image = remove(input_image)
+imagemSemFundo = remove(imagemOriginal)
+
+# Carregar a imagem de fundo
+imagemFundo = Image.open(imagemFundoPath)
+
+# Redimensionar a imagem sem fundo para a mesma dimensão da imagem de fundo
+imagemSemFundo = imagemSemFundo.resize(imagemFundo.size)
+
+# Criar uma nova imagem combinando a imagem de fundo e a imagem sem fundo
+imagemFinal = Image.alpha_composite(imagemFundo.convert('RGBA'), imagemSemFundo)
 
 # Salvar a imagem resultante
-output_image.save(output_path)
+imagemFinal.save(output_path)
+
+print("Imagem final salva com sucesso em:", output_path)
