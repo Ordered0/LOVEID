719a1c16b37458ca04d08978516e869658850811
Rodrigo <rodrigocaiokell@gmail.com>
2024-01-26 17:45:44 -0300

    junção de imagens funcionando

--- js/teste.py
+++ js/teste.py
@@ -3,32 +3,32 @@ import urllib.request
 from PIL import Image
 
 # Obter os parâmetros passados do Node.js
-urllib.request.urlretrieve( 
-    'https://drive.google.com/uc?id=1ryneFJLyt23ZfVDMrpEoxXbZUKdBHrlk', 
-    "gfg.png") 
 
 # Caminho de saída para a imagem resultante
-output_path = "C:/LoveID/js/imagens/imagem_final.png"
+outputPath = "C:/Users/rkoelln/Desktop/codigos pessoais/js/imagens/imagem_final.png"
+
+inputPath = "C:/Users/rkoelln/Desktop/58293.jpg"
 
 # Caminho para a imagem de fundo
-imagemFundoPath = "C:/LoveID/js/imagens/teste.jpg"
-
+urllib.request.urlretrieve( 
+    'https://drive.google.com/uc?id=17pLbv4YEM9wVC-zWX2CvRivfD0v5qM9l',
+    "imagemFundo.png") 
 # Carregar a imagem original
-imagemOriginal = Image.open("gfg.png")
+imagemOriginal = Image.open(inputPath) 
 
 # Remover o fundo usando rembg
-imagemSemFundo = remove(imagemOriginal)
-
+imagemSemFundo = remove(imagemOriginal).convert('RGBA')
+tamanhoSemFundo = (250,270)
 # Carregar a imagem de fundo
-imagemFundo = Image.open(imagemFundoPath)
-
+imagemFundo = Image.open("imagemFundo.png")
 # Redimensionar a imagem sem fundo para a mesma dimensão da imagem de fundo
-imagemSemFundo = imagemSemFundo.resize(imagemFundo.size)
+imagemSemFundo = imagemSemFundo.resize(tamanhoSemFundo)
 
+imagemFundo.paste(imagemSemFundo,(400,10),imagemSemFundo)
 # Criar uma nova imagem combinando a imagem de fundo e a imagem sem fundo
-imagemFinal = Image.alpha_composite(imagemFundo.convert('RGBA'), imagemSemFundo)
+#imagemFinal = Image.alpha_composite(imagemFundo.convert('RGBA'), imagemSemFundo)
 
 # Salvar a imagem resultante
-imagemFinal.save(output_path)
+imagemFundo.save(outputPath, quality=95)
 
-print("Imagem final salva com sucesso em:", output_path)
+print("Imagem final salva com sucesso em:", outputPath)
