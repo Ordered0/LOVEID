babfad8cd131d5490905286166551eb23ce38342
Rodrigo <rodrigocaiokell@gmail.com>
2024-01-30 14:53:46 -0300

    commit testando implementacao botao google

--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require('path');
-const { PythonShell } = require('python-shell');
+// const { PythonShell } = require('python-shell');
 const {DemoGeneric} = require('./demo-generic.js');
 let Google = new DemoGeneric();
 const Apple = require('./apple.js');
@@ -21,35 +21,35 @@ const class_suffix = "testeclube";
 
 app.post('/submit', (req, res) => {
     const valuesFromHTML = req.body;
-    // console.log('Values from HTML:', valuesFromHTML);
-    // let object_suffix = valuesFromHTML.NOME + valuesFromHTML.N_TITULO
-    // Google.createObject(issuer_id, class_suffix, object_suffix,valuesFromHTML.NOME,valuesFromHTML.CATEGORIA,valuesFromHTML.VALIDADE,valuesFromHTML.N_TITULO,valuesFromHTML.EMISSAO);
-    // let LINK = Google.createJwtExistingObjects(issuer_id,object_suffix,class_suffix);
-    // res.json({ message: LINK });
+    console.log('Values from HTML:', valuesFromHTML);
+    let object_suffix = valuesFromHTML.NOME + valuesFromHTML.N_TITULO
+    Google.createObject(issuer_id, class_suffix, object_suffix,valuesFromHTML.NOME,valuesFromHTML.CATEGORIA,valuesFromHTML.VALIDADE,valuesFromHTML.N_TITULO,valuesFromHTML.EMISSAO);
+    let LINK = Google.createJwtExistingObjects(issuer_id,object_suffix,class_suffix);
+    res.json({ message: LINK });
 
-    // Caminhos para as imagens de entrada e saída
-    const inputImagePath = "C:/Users/rkoelln/Desktop/iagovargas.jpg";
-    const outputImagePath = "C:/Users/rkoelln/Desktop/iagovargas2.png";
-    console.log("olaaaaa")
-    // Opções para o script Python
-    const options = {
-        mode: 'text',
-        pythonOptions: ['-u'], // unbuffered output
-        scriptPath: path.join(__dirname),
-        args: [inputImagePath, outputImagePath],
-    };
-    console.log(options.scriptPath)
-    // Executar o script Python
-    PythonShell.run('teste.py', options, function (err, results) {
-        if (err) {
-            console.error('Erro ao executar o script Python:', err);
-            res.status(500).json({ error: 'Erro interno do servidor' });
-        } else {
-            console.log('Resultados do script Python:', results);
-            // Enviar uma resposta ao cliente
-            res.json({ message: 'Processamento concluído com sucesso' });
-        }
-    });
+    // // Caminhos para as imagens de entrada e saída
+    // const inputImagePath = "C:/Users/rkoelln/Desktop/iagovargas.jpg";
+    // const outputImagePath = "C:/Users/rkoelln/Desktop/iagovargas2.png";
+    // console.log("olaaaaa")
+    // // Opções para o script Python
+    // const options = {
+    //     mode: 'text',
+    //     pythonOptions: ['-u'], // unbuffered output
+    //     scriptPath: path.join(__dirname),
+    //     args: [inputImagePath, outputImagePath],
+    // };
+    // console.log(options.scriptPath)
+    // // Executar o script Python
+    // PythonShell.run('teste.py', options, function (err, results) {
+    //     if (err) {
+    //         console.error('Erro ao executar o script Python:', err);
+    //         res.status(500).json({ error: 'Erro interno do servidor' });
+    //     } else {
+    //         console.log('Resultados do script Python:', results);
+    //         // Enviar uma resposta ao cliente
+    //         res.json({ message: 'Processamento concluído com sucesso' });
+    //     }
+    // });
 });
 
 
